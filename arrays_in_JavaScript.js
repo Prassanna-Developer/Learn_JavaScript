520b1d4fb687f06e772d7f3bf008d0ac0b8f2abc
Prassanna , Java Developer , OCJP Certified, CSM Certified <iamprassanna@gmail.com>
2016-05-25 11:53:31 +0530

    Update code to test wrapper objects and array concepts in JS

---
+++ arrays_in_JavaScript.js
@@ -0,0 +1,34 @@
+/**  Playing with Arrays */
+
+
+var myArray = [100,200,300];
+
+console.log(myArray);
+
+console.log(myArray[4]);
+
+
+myArray["4"] = "Value 4";
+
+
+console.log(myArray);
+
+
+myArray[6] = "Value 400";
+
+console.log(myArray);
+
+
+
+
+myArray["foo"] = "abc";
+
+
+
+// Getting the Length
+console.log("Getting Array Length >> " + myArray.length);
+
+var myArray2 = myArray;
+
+console.log( myArray2);
+
---
+++ wrapper_Object.js
@@ -0,0 +1,17 @@
+/**Wrapper Objects*/
+
+var displayName = "Prassanna S";
+
+
+console.log(displayName.length);
+
+var flag = false;
+
+
+console.log(flag);
+
+
+
+var num = 20.57667;
+
+console.log(num);
