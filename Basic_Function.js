ea4ce8e5f97a6780f09de36ec313132bc32e2a4c
Prassanna , Java Developer , OCJP Certified, CSM Certified <iamprassanna@gmail.com>
2016-05-26 02:56:38 +0530

    Uploading Basic Function

---
+++ Basic_Function.js
@@ -0,0 +1,12 @@
+/** Basic Function*/
+function sayHello(name,id){
+  if(name === 'JP')
+    return;
+  return "Say hello to " + name + ".." + id;
+}
+
+
+sayHello("Prassanna");
+
+var returnedValue = sayHello("JP",14);
+
